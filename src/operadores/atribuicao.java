a764595ae3fa49a41b7d4c2d8ce66326275b7824
Andriw Gabryel <agfmondego5@gmail.com>
2026-02-25 14:33:04 -0300

    Atualizações.

--- src/operadores/atribuicao.java
+++ src/operadores/atribuicao.java
@@ -7,17 +7,21 @@ public class atribuicao {
 
         double bonus = 1800.00;
         bonus += 1000.00;
-        double aposbonus = bonus;
-        System.out.println(aposbonus);
-        
-        aposbonus -= 100.00;
-        double aposdesconto = aposbonus;
-        System.out.println(aposdesconto);
+        bonus -= 1000.00;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println("Bonus: " + bonus);
 
-
-
-
-
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        //colocando antes da variável, o incremento ou decremento é feito antes da variável ser usada, ou seja, é adicionado ou subtraído antes
+        //de aparecer
+        System.out.println("Contador: " + contador);
 
     }
 }
