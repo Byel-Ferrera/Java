009ffc65461ef536c85c81292f5c602033a8b689
Andriw Gabryel <agfmondego5@gmail.com>
2026-02-25 14:26:04 -0300

    Aula de Operadores (Atribuição).

---
+++ src/operadores/atribuicao.java
@@ -0,0 +1,23 @@
+package operadores;
+
+public class atribuicao {
+    public static void main(String[] args) {
+        
+        // = += -= *= /= %=
+
+        double bonus = 1800.00;
+        bonus += 1000.00;
+        double aposbonus = bonus;
+        System.out.println(aposbonus);
+        
+        aposbonus -= 100.00;
+        double aposdesconto = aposbonus;
+        System.out.println(aposdesconto);
+
+
+
+
+
+
+    }
+}
